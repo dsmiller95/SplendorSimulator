c7373feec5fbbed78ecf1268cf8950711d43ef75
nephina <alexmiller91@gmail.com>
2022-12-26 15:37:23 -0600

    Added model and finalized data shapes

--- game_model/AI_model/model.py
+++ game_model/AI_model/model.py
@@ -2,19 +2,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def SplendidSplendorModel(nn.Module):
+class SplendidSplendorModel(nn.Module):
     def __init__(self, hidden_layers_width, hidden_layers_num):
         super().__init__()
-        self.hidden_layers_num = hidden_layers_num
-        self.hidden_layers_width = hidden_layers_width
+        self.hidden_num = hidden_layers_num
+        self.hidden_width = hidden_layers_width
 
-        self.in_width = (12*4)+()
+        self.in_width = (4*(6+5+(3*(5+5+1))))+(4*(5+1))+(4*(5+5+5)) #260
+        self.out_width = (1+(3*4)+1+3+1+1+1+1+6) #27
 
+        self.in_layer = nn.Linear(in_features = self.in_width, out_features = self.hidden_width)
+        self.in_activation = nn.ReLU()
+        
+        hidden_layer = nn.Sequential(nn.Linear(self.hidden_width,self.hidden_width),nn.ReLU())
+        self.hidden_layers = nn.ModuleList([hidden_layer for i in range(self.hidden_num)])
+        self.out_layer = nn.Linear(in_features = self.hidden_width, out_features = self.out_width)
+        self.out_activation = nn.ReLU()
 
     def init_weights(self):
+        #initialize with random noise
         for m in self.modules():
             nn.init.orthogonal_(m.weight, val)
             torch.nn.utils.weight_norm(m)
     
     def forward(self,input):
-        return input
+        output = self.in_activation(self.in_layer(input))
+        for layer in enumerate(self.hidden_layers):
+            output = layer(output)
+        output = self.out_activation(self.out_layer(output))
+        return output
