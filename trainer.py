ce9319a3339f861acdc242bc0be416ad6675a8db
Dan Miller <dsmiller95@gmail.com>
2022-12-26 21:20:07 -0600

    fixup broken import in trainer.py

--- trainer.py
+++ trainer.py
@@ -1,5 +1,5 @@
 from game_data.game_config_data import GameConfigData
-from game_model.action import Turn, Action_Type
+from game_model.turn import Turn, Action_Type
 from game_model.game import Game
 from game_model.resource_types import ResourceType
 from game_model.AI_model.model import SplendidSplendorModel
