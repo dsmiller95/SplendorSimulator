810cebdb0a3fef0198af93b865f12b29cde8c665
Dan Miller <dsmiller95@gmail.com>
2022-12-28 12:51:04 -0600

    add a new function which will find the best fit of pick 2, if it exists

--- tests/test_mappers.py
+++ tests/test_mappers.py
@@ -3,6 +3,7 @@ from game_model.AI_model.maps import map_from_AI_output
 from game_model.game import Game
 from game_model.game_runner import step_game
 from tests.test_helpers import assert_banks, test_config
+from game_model.turn import Turn,Action_Type
 
 
 def test_fit_best_pick_three():
@@ -25,3 +26,70 @@ def test_fit_best_pick_three():
 
     assert_banks([0, 1, 0, 1, 1], game.players[0].resource_tokens)
     assert_banks([1, 0, 0, 0, 0, 4], game.available_resources)
+
+
+def test_fit_best_pick_three_invalid_then_pick_two():
+    game = Game(2, test_config, force_shuffle=False)
+    output_vector = \
+        [1, .1, 0, 0] + \
+        ([0]*5*3) + \
+        [0] + \
+        [.1, .2, .3, .4, .5] + \
+        [0] + [0] + \
+        [0] * 6
+    game.available_resources = [0, 4, 0, 0, 1, 4]
+    mapped_action = map_from_AI_output(output_vector, game, game.players[0])
+    assert not (mapped_action is None)
+    assert mapped_action.action_type == Action_Type.TAKE_TWO
+
+    assert_banks([0, 0, 0, 0, 0], game.players[0].resource_tokens)
+
+    step_result = step_game(game, mapped_action)
+    assert step_result == None, step_result
+
+    assert_banks([0, 2, 0, 0, 0, 0], game.players[0].resource_tokens)
+    assert_banks([0, 2, 0, 0, 1, 4], game.available_resources)
+
+def test_fit_pick_two():
+    game = Game(2, test_config, force_shuffle=False)
+    output_vector = \
+        [0, 1, 0, 0] + \
+        ([0]*5*3) + \
+        [0] + \
+        [.1, .2, .3, .4, .5] + \
+        [0] + [0] + \
+        [0] * 6
+    game.available_resources = [0, 4, 0, 0, 1, 4]
+    mapped_action = map_from_AI_output(output_vector, game, game.players[0])
+    assert not (mapped_action is None)
+    assert mapped_action.action_type == Action_Type.TAKE_TWO
+
+    assert_banks([0, 0, 0, 0, 0], game.players[0].resource_tokens)
+
+    step_result = step_game(game, mapped_action)
+    assert step_result == None, step_result
+
+    assert_banks([0, 2, 0, 0, 0, 0], game.players[0].resource_tokens)
+    assert_banks([0, 2, 0, 0, 1, 4], game.available_resources)
+
+def test_fit_pick_two_invalid_then_pick_three():
+    game = Game(2, test_config, force_shuffle=False)
+    output_vector = \
+        [.1, 1, 0, 0] + \
+        ([0]*5*3) + \
+        [0] + \
+        [.1, .2, .3, .4, .5] + \
+        [0] + [0] + \
+        [0] * 6
+    game.available_resources = [0, 3, 1, 0, 1, 4]
+    mapped_action = map_from_AI_output(output_vector, game, game.players[0])
+    assert not (mapped_action is None)
+    assert mapped_action.action_type == Action_Type.TAKE_THREE_UNIQUE
+
+    assert_banks([0, 0, 0, 0, 0], game.players[0].resource_tokens)
+
+    step_result = step_game(game, mapped_action)
+    assert step_result == None, step_result
+
+    assert_banks([0, 1, 1, 0, 1, 0], game.players[0].resource_tokens)
+    assert_banks([0, 2, 0, 0, 0, 4], game.available_resources)
