c3f1acdedd7300a70add9c116836402bedb6febc
Dan Miller <dsmiller95@gmail.com>
2022-12-28 13:08:35 -0600

    add new action output model, initialized to default vect lengths

---
+++ game_model/AI_model/action_output.py
@@ -0,0 +1,10 @@
+
+class ActionOutput:
+    def __init__(self):
+        self.action_choice = [None] * 4
+        self.card_buy = [None] * 15
+        self.reserve_buy = [None] * 3
+        self.coin_draw = [None] * 5
+        self.noble_choice = [None] * 5
+        self.discard_choice = [None]
+        self.discard_amounts = [None] * 6
