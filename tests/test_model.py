442eb0e86a761f7c93106b3e3f16cde1e8f031bf
nephina <alexmiller91@gmail.com>
2022-12-28 16:09:28 -0600

    cleanup

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,9 @@
 import torch
 from game_model.AI_model.model import SplendidSplendorModel
+from game_model.AI_model.action_output import ActionOutput
 
 def test_model_setup():
+
     input_dict = {'in1':1,'in2':3,'in3':7}
     output_dict = {'out1':[1,0,0],'out2':[5,0,0]}
 
@@ -12,5 +14,5 @@ def test_model_setup():
     in3_tensor = torch.Tensor([0.0,1.0,2.0,3.0,4.0,5.0,6.0])
     input_tensor_dict = {'in1':in1_tensor,'in2':in2_tensor,'in3':in3_tensor}
     
-    
-    model.forward(input_tensor_dict))
+
+    model.forward(input_tensor_dict)
