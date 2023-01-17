1aacc9e47a46aef8e8eb1d09022ce8ab6a0c1c5d
nephina <alexmiller91@gmail.com>
2023-01-17 11:10:18 -0600

    added placeholder images, changed noble and card placeholders to those images

--- splendor-web-viewer/src/NobleComponent/NobleComponent.tsx
+++ splendor-web-viewer/src/NobleComponent/NobleComponent.tsx
@@ -5,13 +5,22 @@ import { GetTokenFileName } from '../models/resourceTokens';
 
 interface NobleProps {
   nobleId: number;
+  imageOverride?: string;
 }
 
 function NobleComponent(props: NobleProps) {
-  var imageSource = "game_assets/" + props.nobleId.toString().padStart(3, "0") + ".png";
+  function getImageUrl(): string{
+    if(props.imageOverride){
+      return props.imageOverride;
+    }
+    if(props.nobleId){
+      return "game_assets/" + props.nobleId.toString().padStart(3, "0") + ".png";
+    }
+    return "game_assets/Noble-placeholder.png"
+  }
   return (
     <div className="Noble">
-      <img src={imageSource}/> 
+      <img src={getImageUrl()}/> 
     </div>
   );
 }
