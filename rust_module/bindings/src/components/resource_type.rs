0a9ab7f39ba62619af86fe06ba2df7c5d9a162d3
Dan M <dsmiller95@gmail.com>
2023-10-28 12:45:48 -0500

    fix infinite recursion on error type casting

--- rust_module/bindings/src/components/resource_type.rs
+++ rust_module/bindings/src/components/resource_type.rs
@@ -1,3 +1,4 @@
+
 use pyo3::prelude::*;
 use rust_splendor;
 use rust_splendor::constants::{ResourceTokenType, ResourceType};
@@ -17,9 +18,11 @@ pub struct ResourceTokenToTypeConversionError(ResourceTokenType);
 
 impl From<ResourceTokenToTypeConversionError> for PyErr {
     fn from(value: ResourceTokenToTypeConversionError) -> Self {
-        value.into()
+        use pyo3::exceptions::PyValueError;
+        PyValueError::new_err(format!("cannot convert {:?} into a resource token", value.0))
     }
 }
+
 
 impl TryInto<ResourceType> for SplendorResourceType {
     type Error = ResourceTokenToTypeConversionError;
@@ -43,5 +46,27 @@ impl Into<ResourceTokenType> for SplendorResourceType {
             SplendorResourceType::Onyx => ResourceTokenType::CostType(ResourceType::Onyx),
             SplendorResourceType::Gold => ResourceTokenType::Gold,
         }
+    }
+}
+
+
+#[cfg(test)]
+mod test{
+
+    use super::*;
+    #[test]
+    fn test_conversion_error_converts() {
+        pyo3::prepare_freethreaded_python();
+
+        let gold_type = SplendorResourceType::Gold;
+        fn do_convert(resource_type: SplendorResourceType) -> Result<ResourceType, PyErr> {
+            let token : ResourceType = resource_type.try_into()?;
+            Ok(token)
+        }
+
+        let token = do_convert(gold_type);
+        assert!(token.is_err());
+        let err = token.unwrap_err();
+        assert_eq!(err.to_string(), "ValueError: cannot convert Gold into a resource token");
     }
 }
